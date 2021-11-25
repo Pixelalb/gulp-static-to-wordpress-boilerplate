7c5cdb38956edb70122c8d9dcea0613e567966a2
Victor Stegaru <victor@stegaru.com>
2021-11-25 10:12:32 +0200

    Updates updates

--- html/app/js/app.js
+++ html/app/js/app.js
@@ -1,1 +1,219 @@
-/*----------  Your app  ----------*/
+(function ($) {
+	$(document).ready(function () {
+		/* Cleanup main menu */
+		$(".submenu:empty").parent().remove();
+
+		$(".owl-carousel").owlCarousel({
+			items: 1,
+		});
+
+		$(".has-mega-menu > a").on("click", function (e) {
+			e.preventDefault();
+			var $this = $(this);
+			var isOpen = $this.parent(".active");
+
+			hideOpenMenus(e);
+
+			if (isOpen.length == 0) {
+				$this.parent().addClass("active");
+				$("header .main-menu").addClass("mega-menu-open open");
+				$("header .mega-menu").addClass("open");
+			}
+		});
+
+		$(".has-submenu > a").on("click", function (e) {
+			e.preventDefault();
+			var $this = $(this);
+			var isOpen = $this.parent().find(".submenu-wrapper.open");
+
+			hideOpenMenus(e);
+
+			if (isOpen.length == 0) {
+				$this.parent().addClass("active");
+				$this.parent().find(".submenu-wrapper").addClass("open");
+				$("header .main-menu").addClass("submenu-open");
+				$this
+					.parent()
+					.find(".submenu-wrapper .submenu")
+					.addClass("open");
+			}
+		});
+
+		$(".js-open-mobile-menu").on("click", function (e) {
+			e.preventDefault();
+			hideOpenMenus(e);
+			$("header .main-menu").toggleClass("open");
+		});
+
+		$(".js-toggle-site-sections").on("click", function (e) {
+			e.preventDefault();
+			$(this).toggleClass("active");
+			$(".site-sections-nav").toggleClass("open");
+		});
+
+		/* hide open menus */
+		$(document).click(function (e) {
+			hideOpenMenus(e);
+			closeModals(e);
+		});
+
+		$("nav, .mega-menu").click(function (e) {
+			e.stopPropagation();
+		});
+
+		/* Accordion */
+		$(".accordion .title").click(function (e) {
+			$(this).toggleClass("active");
+			$(this).parent().find(".description").toggleClass("active");
+		});
+
+		/* TOC */
+		$(".toc a").click(function (e) {
+			e.preventDefault();
+			var panel = $(this).attr("href");
+
+			$([document.documentElement, document.body]).animate(
+				{
+					scrollTop: $(panel).offset().top,
+				},
+				700
+			);
+		});
+
+		/* Management cards */
+		$(".js-expand-excerpt").click(function (e) {
+			var panel = $(this).parent().parent().parent().attr("id");
+
+			if ($(this).hasClass("has-close-icon")) {
+				$(this).parent().parent().parent().removeClass("active");
+			} else {
+				$(".management-cards .item.active")
+					.removeClass("active")
+					.find(".has-close-icon")
+					.removeClass("has-close-icon")
+					.text("Read More")
+					.addClass("has-arrow");
+				$(this).parent().parent().parent().addClass("active");
+
+				$([document.documentElement, document.body]).animate(
+					{
+						scrollTop: $("#" + panel).offset().top,
+					},
+					700
+				);
+			}
+
+			$(this).toggleClass("has-close-icon has-arrow");
+			$(this).text(
+				$(this).text() == "Read More" ? "Show Less" : "Read More"
+			);
+		});
+
+		/* KVL Modal */
+		$(".js-modal-open").click(function (e) {
+			e.preventDefault();
+			var redirectURL = $(this).attr("href");
+			var fileName = $(this).data("file-name");
+			var modalTitle = $(this).parent().find("h2").html();
+			$(".kvl-modal .modal-title").html(modalTitle);
+			$(".kvl-modal #kvl-whitepaper-title").val(modalTitle);
+			$(".kvl-modal #kvl-modal-redirect").val(redirectURL);
+			$(".kvl-modal #kvl-modal-filename").val(fileName);
+			$(".kvl-modal").addClass("open");
+		});
+
+		$(".js-video-open").click(function (e) {
+			e.preventDefault();
+			var videoURL = $(this).attr("href");
+			var videoID = youtubeParser(videoURL);
+			var modalTitle = $(this).parent().parent().find("h2").html();
+
+			$(".kvl-modal").addClass("has-video");
+			$(".kvl-modal .modal-title").html(modalTitle);
+			$(".kvl-modal .modal-body").html("");
+			$(".kvl-modal .modal-body iframe").remove();
+			$(
+				'<iframe title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; mute; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+			)
+				.attr(
+					"src",
+					"https://www.youtube.com/embed/" + videoID + "?autoplay=1"
+				)
+				.appendTo(".kvl-modal .modal-body");
+
+			$(".kvl-modal").addClass("open");
+		});
+
+		// $(".js-open-image-in-modal").click(function (e) {
+		// 	e.preventDefault();
+		// 	var imageURL = $(this).attr("href");
+		// 	console.log(imageURL);
+
+		// 	$(".kvl-modal").addClass("has-image");
+		// 	$(".kvl-modal .modal-title").remove();
+		// 	$(".kvl-modal .modal-body").html("");
+		// 	$('<img src="">')
+		// 		.attr("src", imageURL)
+		// 		.appendTo(".kvl-modal .modal-body");
+
+		// 	$(".kvl-modal").addClass("open");
+		// });
+
+		$(".js-modal-close").click(function (e) {
+			e.preventDefault();
+			$(".kvl-modal .modal-body iframe").remove();
+			$(".kvl-modal").removeClass("open");
+		});
+
+		function youtubeParser(url) {
+			var regExp =
+				/^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+			var match = url.match(regExp);
+			return match && match[7].length == 11 ? match[7] : false;
+		}
+
+		function closeModals(e) {
+			if (
+				!(
+					$(e.target).closest(".js-video-open").length > 0 ||
+					$(e.target).closest(".js-modal-open").length > 0 ||
+					$(e.target).closest(".js-open-image-in-modal").length > 0 ||
+					$(e.target).closest(".kvl-modal").length > 0
+				)
+			) {
+				$(".kvl-modal").removeClass("open");
+				$(".kvl-modal .modal-body iframe").remove();
+			}
+		}
+
+		function hideOpenMenus(e) {
+			// console.log(e);
+			/* Hide mega menu */
+			// if (!(
+			// 	// $(e.target).closest(".js-open-mobile-menu").length > 0 ||
+			// 	// $(e.target).closest(".has-mega-menu > a").length > 0
+				
+			// 	)) {
+				// $("header .main-menu.open").removeClass("open");
+				$("header .mega-menu").removeClass("open");
+				$("header .main-menu").removeClass(
+					"mega-menu-open submenu-open"
+				);
+				$("header .main-menu .active").removeClass("active");
+				$("header .submenu-wrapper.open")
+					.removeClass("open")
+					.find(".submenu")
+					.removeClass("open");
+				$(".has-mega-menu").parent().removeClass("active");
+
+				/* Hide simple menus */
+				$(".main-menu")
+					.find(".submenu-wrapper.open")
+					.removeClass("open")
+					.find(".submenu")
+					.removeClass("open");
+			// }
+			// $("header .main-menu").removeClass("open");
+		}
+	});
+})(jQuery);
--- html/gulpfile.js
+++ html/gulpfile.js
@@ -1,26 +1,25 @@
-let gulp = require('gulp');
-let sass = require('gulp-dart-sass');
-let browserSync = require('browser-sync');
-// let useref = require('gulp-useref');
-let uglify = require('gulp-uglify');
-let gulpIf = require('gulp-if');
-var fs = require('fs');
-let cleanCSS = require('gulp-clean-css');
-let sourcemaps = require('gulp-sourcemaps');
-let imagemin = require('gulp-imagemin');
-let cache = require('gulp-cache');
-let del = require('del');
-let rsync = require('gulp-rsync');
-var argv = require('yargs').argv;
+let gulp = require("gulp");
+var fileinclude = require("gulp-file-include");
+let sass = require("gulp-dart-sass");
+let browserSync = require("browser-sync");
+let uglify = require("gulp-uglify");
+let gulpIf = require("gulp-if");
+var fs = require("fs");
+let cleanCSS = require("gulp-clean-css");
+let sourcemaps = require("gulp-sourcemaps");
+let cache = require("gulp-cache");
+let del = require("del");
+let rsync = require("gulp-rsync");
+var argv = require("yargs").argv;
 
 // Build aruments
-var isWordpress = (argv.wordpress === undefined) ? false : true;
-var isTheme = (argv.theme === undefined) ? false : true;
+var isWordpress = argv.wordpress === undefined ? false : true;
+var isTheme = argv.theme === undefined ? false : true;
 
 // Get the config
-var config = require('./config.json');
+var config = require("./config.json");
 
-// Get settings 
+// Get settings
 // -----------------
 // -----------------
 var assetsFolder = config.assetsFolder;
@@ -34,175 +33,211 @@ var wpCustomizerFileName = config.wpCustomizerFileName;
 // -----------------
 // -----------------
 
-
 var wordpressLocalThemePath = localWordPressThemesFolder + themeName;
-var wordpressRemoteThemePath = remoteWordPressPath + "/wp-content/themes/" + themeName;
-var wordpressLocalAssetsPath = wordpressLocalThemePath + "/" +  assetsFolder;
+var wordpressRemoteThemePath =
+	remoteWordPressPath + "/wp-content/themes/" + themeName;
+var wordpressLocalAssetsPath = wordpressLocalThemePath + "/" + assetsFolder;
 // var wordpressRemoteAssetsPath = remoteWordPressPath + "/wp-content/themes/"+ themeName + "/" + assetsFolder;
-var customizerFile = setupAssetsFolder + "/"+ wpCustomizerFileName;
+var customizerFile = setupAssetsFolder + "/" + wpCustomizerFileName;
 
-
-// Default Tasks 
+// Default Tasks
 // -----------------
+gulp.task("browserSync", function () {
+	browserSync({
+		server: ["theme-assets"],
+		port: 8000,
+	});
+});
 
-gulp.task('browserSync', function() {
-    browserSync({
-        server: {
-            baseDir: 'app'
-        },
-        port: 8000
-    })
-})
+gulp.task("sass", function () {
+	return gulp
+		.src("app/scss/**/*.scss")
+		.pipe(sourcemaps.init())
+		.pipe(
+			sass({
+				includePaths: require("node-normalize-scss").includePaths,
+			}).on("error", sass.logError)
+		)
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(assetsFolder + "/css"))
+		.pipe(gulpIf(isWordpress, gulp.dest(wordpressLocalAssetsPath + "/css")))
+		.pipe(
+			browserSync.reload({
+				stream: true,
+			})
+		);
+});
 
-gulp.task('sass', function() {
-    return gulp.src('app/scss/**/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            includePaths: require('node-normalize-scss').includePaths
-        }).on('error', sass.logError))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('app/css'))
-        .pipe(gulpIf(isWordpress, gulp.dest(wordpressLocalAssetsPath + '/css')))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
-})
+gulp.task("html", function () {
+	return gulp
+		.src([
+			"app/*.html",
+			"!app/_head.html", // ignore
+			"!app/_header.html", // ignore
+			"!app/_footer.html", // ignore
+		])
+		.pipe(
+			fileinclude({
+				prefix: "@@",
+				basepath: "@file",
+			})
+		)
+		.pipe(gulp.dest(assetsFolder))
+		.pipe(
+			browserSync.reload({
+				stream: true,
+			})
+		);
+});
 
-gulp.task('html', function() {
-    return gulp.src('app/*.html')
-        .pipe(gulp.dest(assetsFolder))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
-})
-
-gulp.task('javascript', function() {
-    return gulp.src('app/js/**/*.js')
-        .pipe(browserSync.reload({
-            stream: true
-        }))
-        .pipe(gulpIf(isWordpress, gulp.dest(wordpressLocalAssetsPath + '/js')));
-})
-
+gulp.task("javascript", function () {
+	return gulp
+		.src("app/js/**/*.js")
+		.pipe(gulp.dest(assetsFolder + "/js"))
+		.pipe(
+			browserSync.reload({
+				stream: true,
+			})
+		)
+		.pipe(gulpIf(isWordpress, gulp.dest(wordpressLocalAssetsPath + "/js")));
+});
 
 // Optimization tasks
 // ------------
-
-gulp.task('compressJS', function() {
-    return gulp.src('app/**/*.js')
-        .pipe(gulpIf('*.js', uglify()))
-        .pipe(gulp.dest(assetsFolder))
-        .pipe(gulpIf(isWordpress, gulp.dest(wordpressLocalAssetsPath)));
+gulp.task("compressJS", function () {
+	return gulp
+		.src(assetsFolder + "/**/*.js")
+		.pipe(gulpIf("*.js", uglify()))
+		.pipe(gulp.dest(assetsFolder))
+		.pipe(gulpIf(isWordpress, gulp.dest(wordpressLocalAssetsPath)));
 });
 
-gulp.task('compressCSS', function() {
-    return gulp.src('app/**/*.css')
-        .pipe(gulpIf('*.css', cleanCSS()))
-        .pipe(gulp.dest(assetsFolder))
-        .pipe(gulpIf(isWordpress, gulp.dest(wordpressLocalAssetsPath)));
+gulp.task("compressCSS", function () {
+	return gulp
+		.src(assetsFolder + "/**/*.css")
+		.pipe(gulpIf("*.css", cleanCSS()))
+		.pipe(gulp.dest(assetsFolder))
+		.pipe(gulpIf(isWordpress, gulp.dest(wordpressLocalAssetsPath)));
 });
 
-gulp.task('images', function() {
-    return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
-        .pipe(cache(imagemin({
-            interlaced: true,
-        })))
-        .pipe(gulp.dest(assetsFolder + '/images'))
-        .pipe(gulpIf(isWordpress, gulp.dest(wordpressLocalAssetsPath + '/images')));
+gulp.task("images", function () {
+	return gulp
+		.src("app/images/**/*.+(png|jpg|jpeg|gif|svg)")
+		.pipe(gulp.dest(assetsFolder + "/images"))
+		.pipe(
+			gulpIf(isWordpress, gulp.dest(wordpressLocalAssetsPath + "/images"))
+		);
 });
-
 
 //Flow tasks
 // --------------
 
-// Copy theme to backup folder 
-gulp.task('copytheme', function() {
-    return gulp.src(wordpressLocalThemePath + "/**/*.*")
-        .pipe(gulp.dest(localWordPressThemeBackupFolder));
-})
+// Copy theme to backup folder
+gulp.task("copytheme", function () {
+	return gulp
+		.src(wordpressLocalThemePath + "/**/*.*")
+		.pipe(gulp.dest(localWordPressThemeBackupFolder));
+});
 
 // Copying the PHP customizer file
-gulp.task('copycustomizer', function(done) {
-    if (fs.existsSync(wordpressLocalThemePath + "/inc/" + wpCustomizerFileName)) {
-        console.log('\x1b[33mThe file ' + wordpressLocalThemePath + '/inc/' + wpCustomizerFileName + ' already exists\x1b[0m');
-        done();
-    } else {
-        return gulp.src(customizerFile)
-            .pipe(gulp.dest(wordpressLocalThemePath + "/inc"))
-    }    
-})
+gulp.task("copycustomizer", function (done) {
+	if (
+		fs.existsSync(wordpressLocalThemePath + "/inc/" + wpCustomizerFileName)
+	) {
+		console.log(
+			"\x1b[33mThe file " +
+				wordpressLocalThemePath +
+				"/inc/" +
+				wpCustomizerFileName +
+				" already exists\x1b[0m"
+		);
+		done();
+	} else {
+		return gulp
+			.src(customizerFile)
+			.pipe(gulp.dest(wordpressLocalThemePath + "/inc"));
+	}
+});
 
 // Copying the PHP customizer file
-gulp.task('includecustomizer', function(cb) {
-    fs.appendFile(wordpressLocalThemePath + "/functions.php", '\r\n\r\n\r\n/**\r\n *Theme customizer\r\n */ \r\n require get_stylesheet_directory() . "/inc/stegaru-customizer.php";', cb);
-})
+gulp.task("includecustomizer", function (cb) {
+	fs.appendFile(
+		wordpressLocalThemePath + "/functions.php",
+		'\r\n\r\n\r\n/**\r\n *Theme customizer\r\n */ \r\n require get_stylesheet_directory() . "/inc/stegaru-customizer.php";',
+		cb
+	);
+});
 
-// Copying js 
-gulp.task('copyapp', function() {
-    return gulp.src('app/**/*.js')
-        .pipe(gulp.dest(assetsFolder))
-        .pipe(gulpIf(isWordpress, gulp.dest(wordpressLocalAssetsPath)));
-})
+// Copying js
+gulp.task("copyapp", function () {
+	return gulp
+		.src("app/**/*.js")
+		.pipe(gulp.dest(assetsFolder))
+		.pipe(gulpIf(isWordpress, gulp.dest(wordpressLocalAssetsPath)));
+});
 
 // Clean task
-gulp.task('clean', function() {
-    return del([assetsFolder + '/**/*', '!' + assetsFolder + '/images', '!' + assetsFolder + '/images/**/*']);
+gulp.task("clean", function () {
+	return del([
+		assetsFolder + "/**/*",
+		"!" + assetsFolder + "/images",
+		"!" + assetsFolder + "/images/**/*",
+	]);
 });
 
 // Watchers
-gulp.task('watch', function() {
-    gulp.watch('app/scss/**/*.scss', gulp.series('sass'));
-    gulp.watch('app/*.html', gulp.series('html'));
-    gulp.watch('app/js/**/*.js',gulp.series('javascript'));
-})
+gulp.task("watch", function () {
+	gulp.watch("app/scss/**/*.scss", gulp.series("sass"));
+	gulp.watch("app/*.html", gulp.series("html"));
+	gulp.watch("app/js/**/*.js", gulp.series("javascript"));
+});
+
+// Build
+// -----
+gulp.task(
+	"build",
+	gulp.series(
+		"clean",
+		"sass",
+		"copyapp",
+		"html",
+		"compressJS",
+		"compressCSS",
+		"images"
+	)
+);
 
 // The Default task
 // -----
-gulp.task('default', 
-    gulp.parallel('watch', 'browserSync')
+gulp.task(
+	"default",
+	gulp.parallel(gulp.series("build", "watch"), "browserSync")
 );
 
 // Setup Customizer
-gulp.task('customizer',
-    gulp.series(
-        'copycustomizer', 
-        'includecustomizer'
-    )
-);
-
-// Build 
-// -----
-gulp.task('build',
-    gulp.series(
-        'clean', 
-        'sass',
-        'copyapp', 
-        'html',
-        'compressJS',
-        'compressCSS',
-        'images'
-    )
-);
+gulp.task("customizer", gulp.series("copycustomizer", "includecustomizer"));
 
 // Deploy to FTP
 // -------------------------
-gulp.task('deploy', function() {
-    if (isTheme) {
-        console.log("Deploying to Wordpress");
-        return gulp.src(wordpressLocalThemePath + '/**')
-        .pipe(rsync({
-            root: wordpressLocalThemePath + '/',
-            hostname: 'stegaru-dev.com',
-            username: 'root',
-            destination: wordpressRemoteThemePath
-        }));
-    } else {
-        return gulp.src(assetsFolder + '/**')
-        .pipe(rsync({
-            root: assetsFolder + '/',
-            hostname: 'stegaru-dev.com',
-            username: 'root',
-            destination: remoteStaticHTMLPath
-        }));
-    }
+gulp.task("deploy", function () {
+	if (isTheme) {
+		console.log("Deploying to Wordpress");
+		return gulp.src(wordpressLocalThemePath + "/**").pipe(
+			rsync({
+				root: wordpressLocalThemePath + "/",
+				hostname: "",
+				username: "root",
+				destination: wordpressRemoteThemePath,
+			})
+		);
+	} else {
+		return gulp.src(assetsFolder + "/**").pipe(
+			rsync({
+				root: assetsFolder + "/",
+				hostname: "stegaru-dev.com",
+				username: "root",
+				destination: remoteStaticHTMLPath,
+			})
+		);
+	}
 });
